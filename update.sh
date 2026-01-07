4e47efa9d94bd421f91213321bac0841385ebcd8
kuriofoolio <kurialancelot@gmail.com>
2026-01-07 14:46:10 +0300

    renamed branch to sk-daraja-v1

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-git branch -m main daraja
+git branch -m daraja sk-daraja-v1
 git fetch origin
-git branch -u origin/daraja daraja
+git branch -u origin/sk-daraja-v1 sk-daraja-v1
 git remote set-head origin -a
